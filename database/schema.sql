3b03c8ee1cec8eba38c4c96bbe4baac12e9a8256
Tendrynyavo <143934345+tendrynyavo@users.noreply.github.com>
2024-01-12 01:42:26 +0300

    database immobilisation

---
+++ database/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE immobilisation(
+   id_immobilisation VARCHAR(50) ,
+   nom VARCHAR(100) ,
+   PRIMARY KEY(id_immobilisation, nom)
+);
--- src/main/java/com/immobilisation/ImmobilisationApplication.java
+++ src/main/java/com/immobilisation/ImmobilisationApplication.java
@@ -1,4 +1,4 @@
-package com.example.immobilisation;
+package com.immobilisation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
