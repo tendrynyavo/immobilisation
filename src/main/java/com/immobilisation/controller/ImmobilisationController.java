4cfcf45f7e4e0e2f9bcf58919473577ec0d643d7
Tendrynyavo <143934345+tendrynyavo@users.noreply.github.com>
2024-01-12 02:26:35 +0300

    Update ImmobilisationController.java

--- src/main/java/com/immobilisation/controller/ImmobilisationController.java
+++ src/main/java/com/immobilisation/controller/ImmobilisationController.java
@@ -1,9 +1,7 @@
 package com.immobilisation.controller;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
