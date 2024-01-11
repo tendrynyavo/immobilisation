379ebff844ea28c1dc02a0d47a8f2dbe5485aecf
Tendrynyavo <143934345+tendrynyavo@users.noreply.github.com>
2024-01-12 01:28:57 +0300

    first commit

---
+++ src/main/java/com/example/immobilisation/ImmobilisationApplication.java
@@ -0,0 +1,13 @@
+package com.example.immobilisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmobilisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmobilisationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/immobilisation/ImmobilisationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.immobilisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmobilisationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
